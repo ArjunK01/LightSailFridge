b8e5f52ca01dba03006c8a059960d2bcac0c9c62
Arjun Kumar <arjunkumar01@gmail.com>
2020-03-21 21:47:07 -0400

    commit

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Item, Category
+from .models import Item
 
 admin.site.register(Item)
-admin.site.register(Category)
